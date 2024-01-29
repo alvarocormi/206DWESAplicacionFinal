fb4fd239e3c34969698ea6cea12450e1b12810a5
alvarocormi <alvarocormin@gmail.com>
2024-01-29 09:43:31 +0100

    Cambiado el titulo de la card del index de Registro

--- view/vRegistro.php
+++ view/vRegistro.php
@@ -1,6 +1,6 @@
 <title>Registro- Alvaro Cordero Miñambres</title>
     <div class="center" style="position: absolute; top: 45%; left: 50%; transform: translate(-50%, -50%);  border-radius: 5px; width: 400px; background: white; box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.05); height: 550px;">
-        <h1 style="text-align: center; padding: 20px 0; font-size: 32px; color: black; font-weight: 500; border-bottom: 1px solid silver;">Login</h1>
+        <h1 style="text-align: center; padding: 20px 0; font-size: 32px; color: black; font-weight: 500; border-bottom: 1px solid silver;">Registro</h1>
         <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" style="margin-bottom: 20px; padding: 0 40px; box-sizing: border-box;">
             <div class="txt_field" style="position: relative; border-bottom: 2px solid #adadad; margin: 40px 0;">
                 <input type="text" id="usuario" name="T01_CodUsuario"  value="<?php echo (isset($_REQUEST['T01_CodUsuario']) ? $_REQUEST['T01_CodUsuario'] : ''); ?>" style="width: 100%; padding: 0 5px; height: 40px; font-size: 16px; border: none; background: none; outline: none;">
